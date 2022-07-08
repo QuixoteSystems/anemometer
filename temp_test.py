80be8601668c4c98d310225f50763edb9a1d2c50
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-07-08 19:34:09 +0200

    main

--- temp_test.py
+++ temp_test.py
@@ -18,8 +18,8 @@ dht22int = adafruit_dht.DHT22(board.D17)
 temperature_int = dht22int.temperature
 humidity_int = dht22int.humidity
 
-temperature_ext = dht22int.temperature
-humidity_ext = dht22int.humidity
+temperature_ext = dht22ext.temperature
+humidity_ext = dht22ext.humidity
 
 fechaHora = time.strftime("%c")
 
@@ -37,27 +37,27 @@ print("Dia: "+ time.strftime("%d/%m/%y") + "  Hora: "+ time.strftime("%H:%M:%S")
 #print(f"------------------")
 #print(f" Sensor Interior")
 #print(f"------------------")
+while True:
+        tempint = "{:.2f}".format(temperature_int)
+        print(f"Temperatura Interior= " + tempint + " C")
 
-tempint = "{:.2f}".format(temperature_int)
-print(f"Temperatura Interior= " + tempint + " C")
+        f.write("interior_temp "+ tempint + "\n")
 
-f.write("interior_temp "+ tempint + "\n")
+        humint = "{:.2f}".format(humidity_int)
+        print(f"Humedad Interior= "+ humint +" %")
 
-humint = "{:.2f}".format(humidity_int)
-print(f"Humedad Interior= "+ humint +" %")
+        f.write("interior_hum "+ humint + "\n")
 
-f.write("interior_hum "+ humint + "\n")
+        tempext = "{:.2f}".format(temperature_ext)
+        print(f"Temperatura Exterior= " + tempext + " C")
 
-tempext = "{:.2f}".format(temperature_ext)
-print(f"Temperatura Exterior= " + tempext + " C")
+        f.write("exterior_temp "+ tempext + "\n")
 
-f.write("exterior_temp "+ tempext + "\n")
+        humext = "{:.2f}".format(humidity_ext)
+        print(f"Humedad Exterior= "+ humext +" %")
 
-humext = "{:.2f}".format(humidity_ext)
-print(f"Humedad Exterior= "+ humext +" %")
+        f.write("exterior_hum "+ humext + "\n")
 
-f.write("exterior_hum "+ humext + "\n")
-
-#       time.sleep(5)
+        time.sleep(10)
 # Cerramos el archivo
-f.close()
+        f.close()
