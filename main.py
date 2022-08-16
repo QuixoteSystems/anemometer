cd7c501d8488d50377c7dd6efed52b69eabcac91
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-08-16 21:57:51 +0200

    master

--- main.py
+++ main.py
@@ -3,15 +3,15 @@
 import sys
 import time
 
-#from anemometer import Anemometer
+from anemometer import Anemometer
 from termometer import Termometer
 
 if __name__ == "__main__":
-    #anemometer = Anemometer()
+    anemometer = Anemometer()
     termometer = Termometer()
 
     ## Inicio lecturas de datos de anemometro y termometros
-    #anemometer.start_read()
+    anemometer.start_read()
     termometer.start_read()
 
     ## Espera de 3 segundos recopilando los primeros datos
@@ -25,12 +25,12 @@ if __name__ == "__main__":
     ## Muestro constantemente los datos recopilados para probar, calibrar o debug
     while True:
         try:
-            #anemometer.start_read()
+            anemometer.start_read()
             termometer.start_read()
 
             ## Cuando ha tomado 5 lecturas devuelve y resetea contadores
             ## para indicar que comienza una nueva medición.
-            count += 1
+            #count += 1
             #print('Contador:', count)
             #if (count % 5) == 0:
                 #print(anemometer.get_all_datas())
--- termometer.py
+++ termometer.py
@@ -48,18 +48,19 @@ class Termometer():
             print(f"Error de lectura del Sensor Interior: {dht_error}")
             time.sleep(30)
             print("Esperamos 30 segundos antes de volver a leer")
-            temperature_int = self.sensor_interior.temperature
-            humidity_int = self.sensor_interior.humidity
+            #temperature_int = self.sensor_interior.temperature
+            #humidity_int = self.sensor_interior.humidity
             
         try:
             temperature_ext = self.sensor_exterior.temperature
             humidity_ext = self.sensor_exterior.humidity
+
         except RuntimeError as dht_error:
             print(f"Error de lectura del Sensor Exterior: {dht_error}")
             print("Esperamos 30 segundos antes de volver a leer")
             time.sleep(30)
-            temperature_ext = self.sensor_exterior.temperature
-            humidity_ext = self.sensor_exterior.humidity
+            #temperature_ext = self.sensor_exterior.temperature
+            #humidity_ext = self.sensor_exterior.humidity
 
         fecha_hora = time.strftime("%c")
 
