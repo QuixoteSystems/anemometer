36e162cdac0d3a40f3417e04ad79df25648504d7
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-08-17 19:28:18 +0200

    master

--- termometer.py
+++ termometer.py
@@ -59,7 +59,7 @@ class Termometer():
 
         except RuntimeError as dht_error:
             print(f"Error de lectura del Sensor Interior: {dht_error}")
-            logger.warning("Error de lectura del Sensor Interior: %s }")
+            logger.warning(f"Error de lectura del Sensor Interior: {dht_error}")
             time.sleep(30)
             print("Esperamos 30 segundos antes de volver a leer")
             #temperature_int = self.sensor_interior.temperature
@@ -71,6 +71,7 @@ class Termometer():
 
         except RuntimeError as dht_error:
             print(f"Error de lectura del Sensor Exterior: {dht_error}")
+            logger.warning(f"Error de lectura del Sensor Exterior: {dht_error}")
             print("Esperamos 30 segundos antes de volver a leer")
             time.sleep(30)
             #temperature_ext = self.sensor_exterior.temperature
@@ -103,8 +104,10 @@ class Termometer():
             #print(f"Humedad Interior= {hum_int} %")
         except UnboundLocalError as dht_error:
             print(f"Sin datos del Sensor Interior: {dht_error}")
+            logger.warning(f"Sin datos del Sensor Interior: {dht_error}")
         except AttributeError as dht_error:
             print(f"Error del Sensor Interior: {dht_error}")
+            logger.warning(f"Error del Sensor Interior: {dht_error}")
         except RuntimeError as dht_error:
             print(f"Error del Sensor Interior: {dht_error}")
         except TypeError as dht_error:
