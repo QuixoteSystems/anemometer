43d07bf19e7a0d064b5abfe8e565e6e8d4ed6f71
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-08-17 08:59:13 +0200

    master

--- termometer.py
+++ termometer.py
@@ -48,6 +48,7 @@ class Termometer():
             print(f"Error de lectura del Sensor Interior: {dht_error}")
             time.sleep(30)
             print("Esperamos 30 segundos antes de volver a leer")
+            exit()
             #temperature_int = self.sensor_interior.temperature
             #humidity_int = self.sensor_interior.humidity
             
@@ -59,6 +60,7 @@ class Termometer():
             print(f"Error de lectura del Sensor Exterior: {dht_error}")
             print("Esperamos 30 segundos antes de volver a leer")
             time.sleep(30)
+            exit()
             #temperature_ext = self.sensor_exterior.temperature
             #humidity_ext = self.sensor_exterior.humidity
 
